3dde7aea1561d8fcb9ec7871980ba0a7cd8a1f18
Vasim Akram <iamvasim786@gmail.com>
2016-05-05 16:50:07 +0530

    Commit new project on github.(May 5th,2016)

---
+++ SpringBlog/src/main/java/com/raysmond/blog/Application.java
@@ -0,0 +1,17 @@
+package com.raysmond.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+/**
+ * @author Vasim<iamvasim786@gmail.com>
+ */
+@SpringBootApplication
+//@EnableCaching
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
